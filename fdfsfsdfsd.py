100c1421a192b3021cf856faa587cc5eaad5b8ef
Anton Khapaev <123292535+Antohachannel@users.noreply.github.com>
2023-04-23 13:30:52 +0500

    Add files via upload

---
+++ fdfsfsdfsd.py
@@ -0,0 +1,42 @@
+
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height): # добавить еще два параметра при создании и задавать размер прямоугольгника для картинки самим
+        super().__init__()
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (wight, height)) # вместе 55,55 - параметры
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+#Игровая сцена:
+back = (200, 255, 255) # цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+clock = time.Clock()
+FPS = 60
+game = True
+
+ball = GameSprite('unnamed (3).png', 200, 200, 4, 50, 50)
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    ball.reset()
+
+    display.update()
+    clock.tick(FPS)
